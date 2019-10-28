b8f9e129a8e60b5e4e3f53429585df5a53e151f7
turmoil2k <unconfigured@null.spigotmc.org>
2019-10-29 02:55:27 +0400

    added 2 player movement use WASD / arrowkeys to test

---
+++ Assets/Scenes/Jumas shit/MovementController2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController2 : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    Vector3 _EulerAngleVelocity;
+    int speed = 20;
+
+    void Start()
+    {
+        _EulerAngleVelocity = new Vector3(0, 100, 0);
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidBody.velocity = transform.forward * speed;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidBody.velocity = -transform.forward * speed;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Quaternion deltaRotation = Quaternion.Euler(-_EulerAngleVelocity * Time.deltaTime);
+            rigidBody.MoveRotation(rigidBody.rotation * deltaRotation);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Quaternion deltaRotation = Quaternion.Euler(_EulerAngleVelocity * Time.deltaTime);
+            rigidBody.MoveRotation(rigidBody.rotation * deltaRotation);
+        }
+    }
+}
