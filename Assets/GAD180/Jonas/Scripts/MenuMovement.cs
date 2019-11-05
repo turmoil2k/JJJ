f7be40f72104ebd860bbaaa8f611ecc6416eab62
Jonas Joshua Noronha <>
2019-11-05 19:55:57 +0400

    Virtual Scene Transition

---
+++ Assets/GAD180/Jonas/Scripts/MenuMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMovement : MonoBehaviour
+{
+    public Transform[] VirtualScenes;
+    public float Speed;
+    public Transform CurrentScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            CurrentScene = VirtualScenes[0];
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            CurrentScene = VirtualScenes[1];
+        }
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, CurrentScene.position, Time.deltaTime * Speed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, CurrentScene.rotation, Time.deltaTime * Speed);
+    }
+}
