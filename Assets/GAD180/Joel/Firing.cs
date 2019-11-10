5bd479be0bafa71327803fc70b81d44b6bee44a0
Joel Thomas <j.thomas@saedubai.com>
2019-11-10 13:47:11 +0400

    Added and completed firing

---
+++ Assets/GAD180/Joel/Firing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firing : MonoBehaviour
+{
+    public GameObject Bullet;
+    public Transform FirePoint;
+    public float Rate = 0.2f;
+    private float Timer = 0f;
+
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space) && Timer > Rate)
+        {
+            Instantiate(Bullet, FirePoint.position, FirePoint.rotation);
+            Timer = 0f;
+        }
+    }
+}
