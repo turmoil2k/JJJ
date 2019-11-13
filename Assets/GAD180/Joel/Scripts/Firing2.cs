e63dff76b91bc73866b605df3ec21b327f5a12f0
Joel Thomas <j.thomas@saedubai.com>
2019-11-13 13:21:39 +0400

    Added Damage and Bullet destroy

--- Assets/GAD180/Joel/Scripts/Firing2.cs
+++ Assets/GAD180/Joel/Scripts/Firing2.cs
@@ -2,20 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Firing : MonoBehaviour
+public class Firing2 : MonoBehaviour
 {
     public GameObject Bullet;
     public Transform FirePoint;
+
     public float Rate = 0.2f;
     private float Timer = 0f;
 
     void Update()
     {
         Timer += Time.deltaTime;
-        if (Input.GetKey(KeyCode.Space) && Timer > Rate)
+        if (Input.GetKey(KeyCode.Mouse0) && Timer > Rate)
         {
-            Instantiate(Bullet, FirePoint.position, FirePoint.rotation);
+            Fire();
             Timer = 0f;
         }
+    }
+
+    void Fire()
+    {
+        GameObject Bullet_Clone = Instantiate(Bullet, FirePoint.position, FirePoint.rotation) as GameObject;
     }
 }
