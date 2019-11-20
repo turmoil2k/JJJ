e214b275f6fa1610f8aa6c75782d656b04811784
turmoil2k <unconfigured@null.spigotmc.org>
2019-11-20 15:45:21 +0400

    Finished destructable wall which also respawns as requested by Mr Ahmad... Next particles

---
+++ Assets/GAD180/Juma/Scripts/GunController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public float bulletSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject instBullet = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+            Rigidbody instBulletRB = instBullet.GetComponent<Rigidbody>();
+            instBulletRB.AddForce(Vector3.forward * bulletSpeed);
+        }
+    }
+}
---
+++ Assets/GAD180/Juma/Scripts/WallHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallHealth : MonoBehaviour
+{
+    public int health = 10;
+    public float timer;
+    public GameObject accessRespawnScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            accessRespawnScript.GetComponent<WallController>().wallBool = false;
+        }
+    }
+
+    public void DamageToWall(int bulletDMG)
+    {
+        health -= bulletDMG;
+    }
+
+    public void HealthRestore(int addHealth)
+    {
+        health += addHealth;
+    }
+}
+
