f3bccc09db36b13ffb1d8d174aae89bfe320e3cb
turmoil2k <unconfigured@null.spigotmc.org>
2019-12-05 17:17:54 +0400

    Added damage to health UI for both players.

---
+++ Assets/HealthScriptUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScriptUI : MonoBehaviour
+{
+    public float PlayersHealth;
+    public Text healthtext;
+    public GameObject playerObj;
+
+    void Update()
+    {
+        PlayersHealth = playerObj.GetComponent<Damage>().health;
+        healthtext.text = "" + PlayersHealth;
+    }
+}
